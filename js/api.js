b1a1ba9da08d25b24df462c087e5d042716fc915
ederramossupervisor <eder.ramos@educador.edu.es.gov.br>
2026-07-09 10:52:26 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,13 +1,18 @@
 const API = (() => {
   const BASE_URL = 'https://script.google.com/macros/s/AKfycbzTqBGdFu372Z_7fXeUuXuqEgaIV-DDTU1n1SDkh6cLrnY3UAaZuXqmXzNMiAvhtCk5pg/exec';
 
-  async function enviar(dados, timeoutMs = 15000) {
+  async function enviar(dados, timeoutMs = 30000) { // timeout maior para upload
     const controller = new AbortController();
     const timer = setTimeout(() => controller.abort(), timeoutMs);
-    const params = new URLSearchParams({ data: JSON.stringify(dados) });
-    const url = `${BASE_URL}?${params.toString()}`;
+
     try {
-      const resp = await fetch(url, { method: 'GET', signal: controller.signal });
+      const resp = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'text/plain;charset=utf-8' },
+        body: JSON.stringify(dados),
+        signal: controller.signal
+      });
+
       if (!resp.ok) throw new Error(`HTTP ${resp.status}`);
       return await resp.json();
     } catch (e) {
