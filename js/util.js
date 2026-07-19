9f87cf484a1cd1671a3db2aafe69b7b75b0bef36
ederramossupervisor <eder.ramos@educador.edu.es.gov.br>
2026-07-19 11:50:13 -0300

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,5 +1,5 @@
 /**
- * Utilitários gerais – Leitura+
+ * Utilitários gerais – Eder Livros
  */
 const Util = {
   qs: (sel) => document.querySelector(sel),
@@ -50,13 +50,6 @@ const Util = {
     return val ? JSON.parse(val) : def;
   },
 
-  /**
-   * Converte links de compartilhamento do Google Drive em links diretos de imagem.
-   * Aceita formatos como:
-   *   https://drive.google.com/file/d/ID/view?usp=sharing
-   *   https://drive.google.com/file/d/ID/view?usp=drive_link
-   * Retorna a URL original caso não seja um link do Drive.
-   */
   converterLinkDrive: function(url) {
     if (!url) return '';
     const regex = /\/file\/d\/([^/]+)\//;
@@ -64,6 +57,23 @@ const Util = {
     if (match && match[1]) {
       return `https://drive.google.com/thumbnail?id=${match[1]}&sz=w1000`;
     }
-    return url; // não é um link do Drive, retorna o original
+    return url;
   }
 };
+
+// Ripple effect global
+document.addEventListener('click', function(e) {
+  const btn = e.target.closest('.btn');
+  if (!btn) return;
+
+  const ripple = document.createElement('span');
+  ripple.className = 'ripple';
+  const rect = btn.getBoundingClientRect();
+  const size = Math.max(rect.width, rect.height);
+  ripple.style.width = ripple.style.height = size + 'px';
+  ripple.style.left = (e.clientX - rect.left - size / 2) + 'px';
+  ripple.style.top = (e.clientY - rect.top - size / 2) + 'px';
+  btn.appendChild(ripple);
+
+  ripple.addEventListener('animationend', () => ripple.remove());
+});
